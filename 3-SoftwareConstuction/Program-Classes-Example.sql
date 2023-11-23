378fbaf8ac600f765669f9dac9c065e2994d4262
sasha socheslo <101970415+socheslo@users.noreply.github.com>
2023-11-23 20:27:43 +0200

    Create Program-Classes-Example.sql

---
+++ 3-SoftwareConstuction/Program-Classes-Example.sql
@@ -0,0 +1,36 @@
+CREATE OR REPLACE TYPE Sportsmen_ AS OBJECT (
+    id INT,        
+    Name VARCHAR(10),       
+    age INT,
+    address VARCHAR(10),
+    NOT FINAL MEMBER PROCEDURE display
+) NOT FINAL 
+/
+CREATE TYPE SportsmenInfo IS TABLE OF Sportsmen_;
+/
+CREATE OR REPLACE TYPE Trainingprogramnotifications UNDER Sportsmen_ (
+    telephone INT, 
+    info SportsmenInfo,       
+    OVERRIDING MEMBER PROCEDURE display
+); 
+/
+CREATE OR REPLACE TYPE Choosingtrainingprogram_ AS OBJECT (
+    ID INT,
+    date_of_training DATE,
+    duration INT,
+    sportsmen VARCHAR(10),
+    MEMBER PROCEDURE display_info
+);
+/
+CREATE OR REPLACE TYPE Expert AS OBJECT (
+    ID INT,
+    name VARCHAR(100),
+    MEMBER PROCEDURE display
+); 
+/
+CREATE OR REPLACE TYPE Answertoonlinemessage AS OBJECT (
+    ID INT,
+    date_online_message DATE,
+    text VARCHAR(100),
+    MEMBER PROCEDURE display
+); 
