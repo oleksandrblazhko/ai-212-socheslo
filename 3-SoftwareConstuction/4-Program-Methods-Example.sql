fbaf238bce7fd2edd17c1d7bb90cbedb1c5c7ae6
sasha socheslo <101970415+socheslo@users.noreply.github.com>
2023-11-27 08:50:59 +0200

    Update 4-Program-Methods-Example.sql

--- 3-SoftwareConstuction/4-Program-Methods-Example.sql
+++ 3-SoftwareConstuction/4-Program-Methods-Example.sql
@@ -1,20 +1,41 @@
-CREATE OR REPLACE FUNCTION expert_selectTrainingProgram
-    p_id IN INT,
-    p_name IN VARCHAR2
+CREATE OR REPLACE FUNCTION selectTrainingProgram (
+    p_sportsman_id IN VARCHAR2,
+    p_program_name IN VARCHAR2,
+    p_duration IN INT
 )
-RETURN BOOLEAN IS
-    v_id expert.id%Type := p_id;
-    v_name expert.name%Type := TRIM(upper(p_name));
-    v_expert_id expert.expert_id%Type;
+RETURN INT IS
+    v_sportsman_id training.sportsman_id%TYPE := TRIM(upper(p_sportsman_id));
+    v_program_name training.program_name%TYPE := TRIM(upper(p_program_name));
+    v_duration training.duration%TYPE := p_duration;
+    v_program_id training.program_id%TYPE;
 BEGIN
-    IF p_id <= 0 THEN
-        RETURN FALSE;
+    -- Умова №1: Довжина рядка ID спортсмена повинна бути до 50 символів.
+    IF LENGTH(v_sportsman_id) > 50 THEN
+        RETURN -2; -- Помилка: ID спортсмена не відповідає умовам.
     END IF;
 
+    -- Умова №2: ID спортсмена повинен бути унікальним.
     BEGIN
-        SELECT expert_id INTO v_expert_id FROM expert WHERE expert_id = v_id AND TRIM(upper(name)) = v_name;
-        RETURN TRUE;
+        SELECT program_id INTO v_program_id FROM training WHERE sportsman_id = v_sportsman_id AND ROWNUM = 1;
+        RETURN -2; -- Помилка: ID спортсмена не відповідає умовам.
     EXCEPTION WHEN NO_DATA_FOUND THEN
-        RETURN FALSE;
+        NULL; -- ID спортсмена відповідає умовам.
     END;
+
+    -- Умова №3: Довжина рядка назви тренувальної програми повинна бути до 100 символів.
+    IF LENGTH(v_program_name) > 100 THEN
+        RETURN -3; -- Помилка: Назва тренувальної програми не відповідає умовам.
+    END IF;
+
+    -- Умова №4: Тривалість тренувальної програми повинна бути більше 5 днів.
+    IF v_duration <= 5 THEN
+        RETURN -4; -- Помилка: Тривалість тренувальної програми не відповідає умовам.
+    END IF;
+
+    -- Вставка запису про обрану тренувальну програму.
+    INSERT INTO training (sportsman_id, program_name, duration) VALUES (v_sportsman_id, v_program_name, v_duration);
+    COMMIT;
+
+    RETURN 1; -- Спортсмен успішно обрав тренувальну вправу.
 END;
+/
